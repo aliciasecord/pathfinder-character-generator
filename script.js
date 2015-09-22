08d04ee593b1f680da4a89daaff3b1a7bf094d2f
Alicia Secord <alicia.m.secord@gmail.com>
2015-09-22 17:03:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,81 @@
+/* Array for random race selection */
 var Race = ['Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Half-Orc', 'Halfling', 'Human']
-var Race = Race[Math.floor(Math.random() * Race.length)];
 
+/* Array for random race selection */
 var Class = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Wizard']
-var Class = Class[Math.floor(Math.random() * Class.length)];
-
-var Level = Math.floor(Math.random() * 20) + 1
 
 $(document).ready(function() {
     /* At the start, the .character div should be hidden */
-    $('.character').hide();
-    $('.guided').hide();
-    $('.hide').hide();
-    
+    $('.hide, #character, #stats, #guided, #again').hide();
     
     /*If they click "Guided Selection" show .character and .level div */
-    $('#guide').click(function() {
-        $('#race').hide();
-        $('#class').hide();
-        $('.guided').fadeIn('fast');
-        $('#choose').fadeOut('fast');
+    $('#guide, #chooseagain').click(function() {
+        $('#character, #race, #class').hide();
+        $('#guided').fadeIn('fast');
+        $('#choose, #again').fadeOut('fast');
     });
     
     /*Guided Path*/
     
+    /* But if it's the second time through with the Try again buttons need to clear the character fields first  */
+    $('#tryagain, #chooseagain').click(function () {
+        $('#characterlevel, #characterrace, #characterclass').empty();
+    });
+    
     /*Select level and append to Character*/
     $('#level span').click(function() {
-        $('#characterlevel').show();
         var LevelSelection = $("#selectlevel option:selected").text();
-        $('#characterlevel').append(LevelSelection); 
+        $('#characterlevel').append('Level ' + LevelSelection); 
         $('#level').fadeOut('fast');
         $('#race').fadeIn('fast');
         $('#ConfirmRace').hide();
     });
     
     /*Select Race*/
-    $('.racepick').click(function() {
-        $('#race ul').fadeOut('fast');
-        $('#race img').fadeOut('fast');
-        $('#race h3').fadeOut('fast');
-        $('#ConfirmRace').fadeIn('fast');
-    });
-    
     $('#Dwarf').click(function() {
         $('.Dwarf').fadeIn('fast');
         var PCrace = 'Dwarf';
+        $('#characterrace').append(PCrace);
     });
     
     $('#Gnome').click(function() {
         $('.Gnome').fadeIn('fast');
         var PCrace = 'Gnome';
+        $('#characterrace').append(PCrace);
     });
     
     $('#Elf').click(function() {
         $('.Elf').fadeIn('fast');
         var PCrace = 'Elf';
+        $('#characterrace').append(PCrace);
     });
     
     $('#Halfling').click(function() {
         $('.Halfling').fadeIn('fast');
         var PCrace = 'Halfling';
+        $('#characterrace').append(PCrace);
     });
     
     $('#Human').click(function() {
         $('.Human').fadeIn('fast');
         var PCrace = 'Human';
+        $('#characterrace').append(PCrace);
     });
     
     $('#Half-Elf').click(function() {
         $('.Half-Elf').fadeIn('fast');
         var PCrace = 'Half-Elf';
+        $('#characterrace').append(PCrace);
     });
     
     $('#Half-Orc').click(function() {
         $('.Half-Orc').fadeIn('fast');
         var PCrace = 'Half-Orc';
+        $('#characterrace').append(PCrace);
     });
     
     $('#ChooseRace').click(function() {
-        var PCrace = Race
+        var PCrace = Race[Math.floor(Math.random() * Race.length)];
         if(PCrace == 'Dwarf') {
     	    $('span.Dwarf').fadeIn('fast');
     	    }
@@ -94,13 +92,21 @@ $(document).ready(function() {
 	        $('span.Half-Elf').fadeIn('fast');
             }
         else if (PCrace == 'Half-Orc') {
-	        $('span.Half-Orc').fadeIn('fast');
+	        $('span.Half-Orc').fadeIn('fast'); 
             }
         else {
 	        $('span.Human').fadeIn('fast');
             }
+        $('#characterrace').append(PCrace);
         $('#race').fadeOut('fast');
         $('#class').fadeIn('fast');
+    });
+    
+    $('.racepick').click(function() {
+        $('#race ul').fadeOut('fast');
+        $('#race img').fadeOut('fast');
+        $('#race h3').fadeOut('fast');
+        $('#ConfirmRace').fadeIn('fast');
     });
     
     $('#SelectRace').click(function() {
@@ -110,74 +116,77 @@ $(document).ready(function() {
     });
     
     $('#ResetRace').click(function() {
-        $('.chosenrace').fadeOut('fast');
+        $('.chosenrace, #ConfirmRace').fadeOut('fast');
         $('#ConfirmRace').fadeOut('fast');
-        $('#race ul').fadeIn('fast');
-        $('#race img').fadeIn('fast');
-        $('#race h3').fadeIn('fast');
+        $('#race ul, #race img, #race h3').fadeIn('fast');
+        $('#characterrace').empty();
     });
     
    /*Select class*/
-    $('.classpick').click(function() {
-        $('#class ul').fadeOut('fast');
-        $('#class img').fadeOut('fast');
-        $('#class h3').fadeOut('fast');
-        $('#ConfirmClass').fadeIn('fast');
-    });
-    
     $('#Barbarian').click(function() {
         $('.Barbarian').fadeIn('fast');
         var PCclass = 'Barbarian';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Bard').click(function() {
         $('.Bard').fadeIn('fast');
         var PCclass = 'Bard';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Cleric').click(function() {
         $('.Cleric').fadeIn('fast');
         var PCclass = 'Cleric';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Druid').click(function() {
         $('.Druid').fadeIn('fast');
         var PCclass = 'Druid';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Fighter').click(function() {
         $('.Fighter').fadeIn('fast');
         var PCclass = 'Fighter';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Monk').click(function() {
         $('.Monk').fadeIn('fast');
         var PCclass = 'Monk';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Paladin').click(function() {
         $('.Paladin').fadeIn('fast');
         var PCclass = 'Paladin';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Ranger').click(function() {
         $('.Ranger').fadeIn('fast');
         var PCclass = 'Ranger';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Rogue').click(function() {
         $('.Rogue').fadeIn('fast');
         var PCclass = 'Rogue';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Wizard').click(function() {
         $('.Wizard').fadeIn('fast');
         var PCclass = 'Wizard';
+        $('#characterclass').append(PCclass);
     });
     
     $('#Sorcerer').click(function() {
         $('.Sorcerer').fadeIn('fast');
         var PCclass = 'Sorcerer';
+        $('#characterclass').append(PCclass);
     });
     
     $('#ChooseClass').click(function() {
@@ -215,34 +224,41 @@ $(document).ready(function() {
         else {
 	        $('span.Sorcerer').fadeIn('fast');
             }
+        $('#characterclass').append(PCclass);
         $('#class').fadeOut('fast');
         $('.character').fadeIn('fast');
+    });
+    
+    $('.classpick').click(function() {
+        $('#class ul, #class img, #class h3').fadeOut('fast');
+        $('#ConfirmClass').fadeIn('fast');
     });
     
     $('#SelectClass').click(function() {
         $('#class').fadeOut('fast');
         $('.character').fadeIn('fast');
+        $('#again').fadeIn('fast');
     });
     
     $('#ResetClass').click(function() {
-        $('.chosenclass').fadeOut('fast');
-        $('#ConfirmClass').fadeOut('fast');
-        $('#class ul').fadeIn('fast');
-        $('#class img').fadeIn('fast');
-        $('#class h3').fadeIn('fast');
+        $('.chosenclass, #ConfirmClass').fadeOut('fast');
+        $('#class ul, #class img, #class h3').fadeIn('fast');
+        $('#characterclass').empty();
     });
     
     /* Random Path */
     
     /* If they choose the Random option, we have to chose level, race and class */
-    $('#random').click(function() {
-        /*Chosse level*/
-        var LevelSelection = Level;
+    
+    $('#random, #tryagain').click(function() {
+        
+        /*Choose level*/
+        var Level = Math.floor(Math.random() * 20) + 1
         $('#characterlevel').show();
-        $('#characterlevel').append(LevelSelection);
+        $('#characterlevel').append('Level ' + Level);
         
         /*Chosse race*/
-        var PCrace = Race
+        var PCrace = Race[Math.floor(Math.random() * Race.length)];
         if(PCrace == 'Dwarf') {
     	    $('span.Dwarf').fadeIn('fast');
     	    }
@@ -264,8 +280,10 @@ $(document).ready(function() {
         else {
 	        $('span.Human').fadeIn('fast');
             }
+        $('#characterrace').append(PCrace);
+        
         /*Choose Class*/
-        var PCclass = Class
+        var PCclass = Class[Math.floor(Math.random() * Class.length)];
         if(PCclass == 'Barbarian') {
     	    $('span.Barbarian').fadeIn('fast');
     	    }
@@ -299,9 +317,24 @@ $(document).ready(function() {
         else {
 	        $('span.Sorcerer').fadeIn('fast');
             }
+        $('#characterclass').append(PCclass);
         /* Then hide the choices and show the character description */
         $('#choose').fadeOut('fast');
         $('.character').fadeIn('fast');
+        
+        /* Also show options to start over or move on */
+        $('#again').fadeIn('fast');
     });
     
+    /* Roll Stats */
+    $('#charnext').click(function() {
+        $('#again').fadeOut('fast');
+        $('#stats').fadeIn('fast');
+        /* Function for rolling stats */
+        var d6 = Math.floor(Math.random() * 6) + 1
+        var statroll = ["d6, d6, d6, d6"];
+        var statroll = statroll.sort();
+        var statroll = 
+        stats = sixstats.sort();
+    });
 });
