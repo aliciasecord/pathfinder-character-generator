be7fa2b0c01328cd12a7fc4c0d9c77dde92a0261
Alicia Secord <alicia.m.secord@gmail.com>
2015-09-27 22:59:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -254,8 +254,8 @@ $(document).ready(function() {
     
     var stats = [10, 12, 14, 14, 16, 20];
     
-    var PCClass = "Barbarian"
-    var PCRace = "Dwarf"
+    var PCClass = "Barbarian";
+    var PCRace = "Dwarf";
     
     /* Calculate STR */
     if (PCClass == 'Barbarian' || PCClass == 'Fighter' || PCClass == 'Monk') {str = stats[5]}
